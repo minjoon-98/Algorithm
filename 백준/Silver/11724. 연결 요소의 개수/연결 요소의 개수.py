7094561a2a2cd70a4d2d7f57c3b511ecbca22e81
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-03-30 17:21:12 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 552 ms, Memory: 38412 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,25 +1,25 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6)    # 런타임 에러 오류 수정
 
-N, M = map(int, sys.stdin.readline().split())   # 1 <= N <= 1000
-graph = [[0]*(N+1) for _ in range(N+1)]  # 수정된 부분
+N, M = map(int, sys.stdin.readline().split())
+graph = [[0]*(N+1) for _ in range(N+1)]
 count = 0
 
 for _ in range(M):
     u, v = map(int, sys.stdin.readline().split())
-    graph[u][v] = graph[v][u] = 1   # 여기서 graph[0]은 존재하지 않음
+    graph[u][v] = graph[v][u] = 1
 
-visited = [False] * (N+1)  # 수정된 부분 따라서 N+1개 만듦
+visited = [False] * (N+1)
 
 def dfs(v):
     visited[v] = True
-    for i in range(1, N+1):  # 수정된 부분 
-        if not visited[i] and graph[v][i] == 1:  # 수정된 부분
+    for i in range(1, N+1):
+        if not visited[i] and graph[v][i] == 1:
             dfs(i)
 
-for j in range(1, N+1):  # 수정된 부분
-    if visited[j] == False:
-        dfs(j)
+for i in range(1, N+1):
+    if visited [i] == False:
+        dfs(i)
         count += 1
 
 print(count)
