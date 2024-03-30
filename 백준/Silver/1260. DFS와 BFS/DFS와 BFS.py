6e0998b961790f437fb45f292c268d9156d6d8de
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-03-30 15:58:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,32 +2,39 @@ from collections import deque
 import sys
 
 N, M, V = map(int, sys.stdin.readline().split())
-graph = [[0]*(N+1) for _ in range(N+1)]
 
+# 그래프의 인접 리스트 생성
+graph = [[] for _ in range(N + 1)]
 for _ in range(M):
-    x, y = map(int, sys.stdin.readline().split())
-    graph[x][y] = graph[y][x] = 1
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-visitedDFS = [False] * (N+1)
-visitedBFS = [False] * (N+1)
+# DFS 및 BFS에 사용할 방문 여부 리스트
+visitedDFS = [False] * (N + 1)
+visitedBFS = [False] * (N + 1)
 
 def dfs(v):
     visitedDFS[v] = True
     print(v, end=' ')
-    for i in range(N+1):
-        if not visitedDFS[i] and graph[v][i] == 1:
+    for i in graph[v]:
+        if not visitedDFS[i]:
             dfs(i)
 
 def bfs(v):
-    q = deque([v])
+    queue = deque([v])
     visitedBFS[v] = True
-    while q:
-        v = q.popleft()
+    while queue:
+        v = queue.popleft()
         print(v, end=' ')
-        for i in range(N+1):
-            if not visitedBFS[i] and graph[v][i] == 1:
-                q.append(i)
+        for i in graph[v]:
+            if not visitedBFS[i]:
+                queue.append(i)
                 visitedBFS[i] = True
+
+# DFS 및 BFS 시작 전에 방문 여부 초기화
+for i in range(1, N + 1):
+    graph[i].sort()
 
 dfs(V)
 print()
