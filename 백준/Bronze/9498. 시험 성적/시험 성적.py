c29650c08c8462514f1a85cbec882ffbb0a03f45
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-04-01 00:30:06 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input(''))
+
+if 100 >= score >= 90 :
+    print('A')
+if 90 > score >= 80 :
+    print('B')
+if 80 > score >= 70 :
+    print('C')
+if 70 > score >= 60 :
+    print('D')
+if 60 > score >= 0 :
+    print('F')
