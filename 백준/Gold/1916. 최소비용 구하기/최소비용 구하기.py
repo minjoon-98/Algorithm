1dab05671f54e972d2f3d8032219485dbfa873a9
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-04-03 15:02:00 +0900

    [Gold V] Title: 최소비용 구하기, Time: 292 ms, Memory: 57372 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,32 +1,32 @@
 import heapq
 import sys
 
-N = int(sys.stdin.readline())
-M = int(sys.stdin.readline())
+inf = float('inf')
 
-INF = float('inf')
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
 
-graph = [[] for _ in range(N+1)]
-distance = [INF] * (N+1)
+graph = [[] for _ in range(n+1)]
+distance = [inf] * (n+1)
 
-for _ in range(M):
+for _ in range(m):
     a, b, cost = map(int, sys.stdin.readline().split())
-    graph[a].append([b, cost])
+    graph[a].append((b,cost))
 
 start, finish = map(int, sys.stdin.readline().split())
 
 def dijkstra(start):
-    priority_queue = []
-    heapq.heappush(priority_queue, (0, start))
+    pq = []
     distance[start] = 0
-    while priority_queue:
-        dist, curr = heapq.heappop(priority_queue)
-        if distance[curr] < dist: continue
-        for i in graph[curr]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(priority_queue, (cost, i[0]))
+    heapq.heappush(pq, (0, start))    # 가중치 순으로 정렬해야하기 떄문에 가중치를 앞에 오도록 작성
+    while pq:
+        dist, curr = heapq.heappop(pq)
+        if distance[curr] < dist : continue # 현재 노드까지의 저장된 거리가 우선순위 큐에서 꺼낸 거리보다 작다면 이미 작은 값을 저장하고 있기 때문에, 값을 갱신하지 않고 넘김
+        for next_node, next_cost in graph[curr]:
+            cost = dist + next_cost
+            if cost < distance[next_node]:
+                distance[next_node] = cost
+                heapq.heappush(pq, (cost, next_node))
     return distance[finish]
 
 print(dijkstra(start))
