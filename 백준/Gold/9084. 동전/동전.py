d9d1f7120d9777df8a8f966ecc582d42c1dd30e8
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-04-06 15:33:27 +0900

    [Gold V] Title: 동전, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,40 @@
+# 1차원 DP table 코드
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    M = int(sys.stdin.readline())
+
+    dp = [0] * (M+1)
+    dp[0] = 1
+    for coin in coins:
+        for i in range(1, M+1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
+    print(dp[M])
+
+
+# # 2차원 DP table 코드
+
+# import sys
+
+# T = int(sys.stdin.readline())
+
+# for _ in range(T):
+#     N = int(sys.stdin.readline())
+#     coins = list(map(int, sys.stdin.readline().split()))
+#     coins.insert(0,0)
+#     M = int(sys.stdin.readline())
+#     dp = [[0] * (M+1) for _ in range(N+1)]
+#     for i in range(N+1):
+#         dp[i][0] = 1
+#     for i in range(1, N+1):
+#         for j in range(M+1):
+#             dp[i][j] = dp[i-1][j]
+#             if j - coins[i] >= 0:
+#                 dp[i][j] += dp[i][j-coins[i]]
+#     print(dp[N][M])
