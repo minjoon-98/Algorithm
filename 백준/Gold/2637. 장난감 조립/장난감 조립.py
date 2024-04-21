71fd7ec07316ff2c1bd62cfef2c9eea4d2175956
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-04-21 20:20:49 +0900

    [Gold II] Title: 장난감 조립, Time: 64 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -5,33 +5,30 @@ N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 
 graph = [[] for _ in range(N+1)]
-in_degree = [0 for _ in range(N+1)]
-needs = [[0]*(N+1) for _ in range(N+1)]
-que = deque()
+indegree = [0] * (N+1)
 
 for _ in range(M):
-    X, Y, K = map(int, sys.stdin.readline().split())
-    graph[Y].append((X, K))
-    in_degree[X] += 1
+    post, pre, weight = map(int, sys.stdin.readline().split())
+    graph[pre].append((post, weight))
+    indegree[post] += 1
 
-# 진입 차수가 0인 부품을 큐에 넣음
+dp = [[0]*(N+1) for _ in range(N+1)]
+q = deque()
+
 for i in range(1, N+1):
-    if in_degree[i] == 0:
-        que.append(i)
+    if (indegree[i] == 0):
+        dp[i][i] = 1
+        q.append(i)
 
-# 위상 정렬 시작
-while que:
-    curr = que.popleft()
-    for next, required_amount in graph[curr]:
-        # 만약 현 제품이 기본 부품이면 (needs 행렬에 0이 N+1개 배열됨)
-        if needs[curr].count(0) == N + 1:
-            needs[next][curr] += required_amount
-        # 현 제품이 중간 부품이면
-        for i in range(1, N+1):
-            needs[next][i] += needs[curr][i] * required_amount
-        in_degree[next] -= 1
-        if in_degree[next] == 0:
-            que.append(next)
-for parts in enumerate(needs[N]):
-    if parts[1] > 0:
-        print(*parts)
+while q:
+    pre = q.popleft()
+    for post, weight in graph[pre]:
+        for j in range(1, N+1):
+            dp[post][j] += dp[pre][j] * weight
+        indegree[post] -= 1
+        if indegree[post] == 0:
+            q.append(post)
+
+for i in range(1, N):
+    if dp[N][i]:
+        print(i, dp[N][i])
