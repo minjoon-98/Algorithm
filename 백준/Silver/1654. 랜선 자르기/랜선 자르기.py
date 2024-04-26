b90733a13f5c8512ed0baa536da82a11fcd6adf5
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-04-26 10:13:52 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,23 @@
+import sys
+
+K, N = map(int, sys.stdin.readline().split())
+cables = []
+
+for _ in range(K):
+    cables.append(int(sys.stdin.readline()))
+
+start = 0
+end = (sum(cables) // N)+1
+
+while start <= end:
+    count = 0
+
+    mid = (start + end) // 2
+    for i in range(K):
+        count += cables[i] // mid
+    if N <= count:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
