b08746213a9cc812eee0e319b9241a6bd5882a36
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-06-07 10:14:20 +0900

    [Silver I] Title: 볼 모으기, Time: 224 ms, Memory: 39544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -1,10 +1,16 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 RB_balls = list(input().strip())
+RB_balls_1 = deque()
+
+for i in range(N):
+    RB_balls_1.append(RB_balls[i])
 
 last_color = RB_balls[-1]
+first_color = RB_balls[0]
 
 for i in range(N-1, -1, -1):
     if RB_balls[i] == last_color:
@@ -12,9 +18,19 @@ for i in range(N-1, -1, -1):
     else:
         break
 
+while RB_balls_1:
+    if RB_balls_1[0] == first_color:
+        RB_balls_1.popleft()
+    else:
+        break
+
 count_R = RB_balls.count('R')
 count_B = RB_balls.count('B')
 
-answer = min(count_R, count_B)
+count_R_1 = RB_balls_1.count('R')
+count_B_1 = RB_balls_1.count('B')
+
+
+answer = min(count_R, count_B, count_R_1, count_B_1)
 
 print(answer)
