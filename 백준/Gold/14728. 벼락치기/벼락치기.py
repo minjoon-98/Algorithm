27578ece8c635a1ef764dede4b73c022b8164093
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-10-24 17:02:48 +0900

    [Gold V] Title: 벼락치기, Time: 548 ms, Memory: 52796 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -3,20 +3,18 @@ input = sys.stdin.readline
 
 N, T = map(int, input().split())
 
-time_score = [[]]
+time_score = []
 for i in range(N):
     time_score.append(tuple(map(int, input().split())))
 
-# time_score.sort(key=lambda x : (-x[1], x[0]))
-# time_score.sort(key=lambda x : (-(x[1]/x[0])))
-knapsack = [[0] * (T+1) for _ in range(N+1)]
+knapsack = [[0] * (T + 1) for _ in range(N + 1)]
 
-for i in range(1, N+1):
-    for j in range(1, T+1):
-        time, score = time_score[i]
+for i in range(1, N + 1):
+    time, score = time_score[i - 1]
+    for j in range(1, T + 1):
         if j < time:
-            knapsack[i][j] = knapsack[i-1][j]
+            knapsack[i][j] = knapsack[i - 1][j]
         else:
-            knapsack[i][j] = max(knapsack[i-1][j], knapsack[i-1][j-time] + score)
-        
+            knapsack[i][j] = max(knapsack[i - 1][j], knapsack[i - 1][j - time] + score)
+
 print(knapsack[N][T])
