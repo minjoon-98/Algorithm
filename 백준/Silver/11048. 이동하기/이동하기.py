65222bbb9576cd3e377814c0082c119d549fb9f9
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2024-12-05 14:44:52 +0900

    [Silver II] Title: 이동하기, Time: 736 ms, Memory: 78604 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -3,16 +3,10 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 maze = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0]*M for _ in range(N)]
+dp = [[0]*(M+1) for _ in range(N+1)]
 
-dp[0][0] = maze[0][0]
-for i in range(N):
-    for j in range(M):
-        if i > 0 and j > 0:
-            dp[i][j] = maze[i][j] + max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
-        elif i > 0:
-            dp[i][j] = maze[i][j] + dp[i-1][j]
-        elif j > 0:
-            dp[i][j] = maze[i][j] + dp[i][j-1]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+            dp[i][j] = maze[i-1][j-1] + max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
 
-print(dp[N-1][M-1])
+print(dp[N][M])
