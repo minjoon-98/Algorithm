6b0eb7e11f5134450d6d628e783d972825634f0e
minjoon-98 <155042110+minjoon-98@users.noreply.github.com>
2025-04-07 15:09:33 +0900

    [D3] Title: 0/1 Knapsack, Time: 121 ms, Memory: 33,208 KB -BaekjoonHub

--- Java/SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ Java/SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -25,23 +25,70 @@ public class Solution {
                 C[i] = Integer.parseInt(st.nextToken());    
             }
 
-            int[][] knapsack = new int[N+1][K+1];
+            int[] knapsack = new int[K+1];
             
             for (int i = 1; i <= N; i++) {
-				for (int k = 1; k <= K; k++) {
+				for (int k = K; k >= V[i]; k--) {
 					if (V[i] <= k) {
-						knapsack[i][k] = Math.max(knapsack[i-1][k], knapsack[i-1][k - V[i]] + C[i]);						
-					} else {
-						knapsack[i][k] = knapsack[i-1][k];
+						knapsack[k] = Math.max(knapsack[k], knapsack[k - V[i]] + C[i]);						
 					}
 					
 				}
 			}
             
-            sb.append("#").append(tc).append(" ").append(knapsack[N][K]).append("\n");
+            sb.append("#").append(tc).append(" ").append(knapsack[K]).append("\n");
         }
         
         System.out.println(sb.toString());
     }
 }
 
+
+
+//package a0407.homework;
+//
+//import java.io.*;
+//import java.util.*;
+//
+//public class Solution_d4_5643_키순서_서울_14반_김민준 {
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st = null;
+//        int T = Integer.parseInt(br.readLine());
+//        
+//        for (int tc = 1; tc <= T; tc++) {
+//        	st = new StringTokenizer(br.readLine());
+//            int N = Integer.parseInt(st.nextToken());
+//            int K = Integer.parseInt(st.nextToken());
+//            
+//            int[] V = new int[N+1];	// 부피
+//            int[] C = new int[N+1];	// 가치
+//            
+//            for (int i = 1; i <= N; i++) {
+//                st = new StringTokenizer(br.readLine());
+//                V[i] = Integer.parseInt(st.nextToken());
+//                C[i] = Integer.parseInt(st.nextToken());    
+//            }
+//
+//            int[][] knapsack = new int[N+1][K+1];
+//            
+//            for (int i = 1; i <= N; i++) {
+//				for (int k = 1; k <= K; k++) {
+//					if (V[i] <= k) {
+//						knapsack[i][k] = Math.max(knapsack[i-1][k], knapsack[i-1][k - V[i]] + C[i]);						
+//					} else {
+//						knapsack[i][k] = knapsack[i-1][k];
+//					}
+//					
+//				}
+//			}
+//            
+//            sb.append("#").append(tc).append(" ").append(knapsack[N][K]).append("\n");
+//        }
+//        
+//        System.out.println(sb.toString());
+//    }
+//}
+//
